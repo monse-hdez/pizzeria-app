4e309cbb46951574bf96cf3be6af4978a41d8ff3
monse-hdez <danielamonserrathernandezbalca@gmail.com>
2026-04-23 10:28:31 -0600

    lo que hice

--- app/customize.tsx
+++ app/customize.tsx
@@ -47,7 +47,7 @@ export default function Customize() {
       {/* Título */}
       <Text style={styles.title}>Arma tu Pizza 🍕</Text>
 
-      {/* Contenedor de pizza con ingredientes encima */}
+      {/* Contenedor de pizza */}
       <View style={styles.pizzaContainer}>
         {/* Pizza base */}
         <Image
@@ -56,49 +56,103 @@ export default function Customize() {
           resizeMode="contain"
         />
 
-        {/* Ingredientes encima de la pizza */}
+        {/* QUESO */}
         {seleccionados.includes("Queso") && (
-          <Image
-            source={require("../assets/images/ingredientes/cheese.png")}
-            style={styles.overlay}
-            resizeMode="contain"
-          />
+          <>
+            <Image
+              source={require("../assets/images/ingredientes/cheese.png")}
+              style={styles.queso1}
+              resizeMode="contain"
+            />
+            <Image
+              source={require("../assets/images/ingredientes/cheese.png")}
+              style={styles.queso2}
+              resizeMode="contain"
+            />
+            <Image
+              source={require("../assets/images/ingredientes/cheese.png")}
+              style={styles.queso3}
+              resizeMode="contain"
+            />
+          </>
         )}
 
+        {/* PEPPERONI */}
         {seleccionados.includes("Pepperoni") && (
-          <Image
-            source={require("../assets/images/ingredientes/pepperoni.png")}
-            style={styles.overlay}
-            resizeMode="contain"
-          />
+          <>
+            <Image
+              source={require("../assets/images/ingredientes/pepperoni.png")}
+              style={styles.pepperoni1}
+              resizeMode="contain"
+            />
+            <Image
+              source={require("../assets/images/ingredientes/pepperoni.png")}
+              style={styles.pepperoni2}
+              resizeMode="contain"
+            />
+            <Image
+              source={require("../assets/images/ingredientes/pepperoni.png")}
+              style={styles.pepperoni3}
+              resizeMode="contain"
+            />
+          </>
         )}
 
+        {/* JALAPEÑOS */}
         {seleccionados.includes("Jalapeños") && (
-          <Image
-            source={require("../assets/images/ingredientes/chilli.png")}
-            style={styles.overlay}
-            resizeMode="contain"
-          />
+          <>
+            <Image
+              source={require("../assets/images/ingredientes/chilli.png")}
+              style={styles.jalapeno1}
+              resizeMode="contain"
+            />
+            <Image
+              source={require("../assets/images/ingredientes/chilli.png")}
+              style={styles.jalapeno2}
+              resizeMode="contain"
+            />
+            <Image
+              source={require("../assets/images/ingredientes/chilli.png")}
+              style={styles.jalapeno3}
+              resizeMode="contain"
+            />
+          </>
         )}
 
+        {/* PIÑA */}
         {seleccionados.includes("Piña") && (
-          <Image
-            source={require("../assets/images/ingredientes/pina.png")}
-            style={styles.overlay}
-            resizeMode="contain"
-          />
+          <>
+            <Image
+              source={require("../assets/images/ingredientes/pina.png")}
+              style={styles.pina1}
+              resizeMode="contain"
+            />
+            <Image
+              source={require("../assets/images/ingredientes/pina.png")}
+              style={styles.pina2}
+              resizeMode="contain"
+            />
+          </>
         )}
 
+        {/* PHILADELPHIA */}
         {seleccionados.includes("Philadelphia") && (
-          <Image
-            source={require("../assets/images/ingredientes/philadelphia.png")}
-            style={styles.overlay}
-            resizeMode="contain"
-          />
+          <>
+            <Image
+              source={require("../assets/images/ingredientes/philadelphia.png")}
+              style={styles.phila1}
+              resizeMode="contain"
+            />
+            <Image
+              source={require("../assets/images/ingredientes/philadelphia.png")}
+              style={styles.phila2}
+              resizeMode="contain"
+            />
+          </>
         )}
       </View>
 
-      {/* Ingredientes seleccionables */}
+      {/* Ingredientes */}
       <View style={styles.grid}>
         {ingredientes.map((item, index) => (
           <TouchableOpacity
@@ -145,7 +199,6 @@ const styles = StyleSheet.create({
     color: "#6B3E26",
   },
 
-  /* Contenedor de pizza */
   pizzaContainer: {
     width: 250,
     height: 250,
@@ -161,11 +214,113 @@ const styles = StyleSheet.create({
     position: "absolute",
   },
 
-  /* Ingredientes encima */
-  overlay: {
-    width: 250,
-    height: 250,
+  /* QUESO */
+  queso1: {
     position: "absolute",
+    width: 45,
+    height: 45,
+    top: 40,
+    left: 50,
+  },
+
+  queso2: {
+    position: "absolute",
+    width: 40,
+    height: 40,
+    top: 120,
+    right: 60,
+  },
+
+  queso3: {
+    position: "absolute",
+    width: 42,
+    height: 42,
+    bottom: 50,
+    left: 90,
+  },
+
+  /* PEPPERONI */
+  pepperoni1: {
+    position: "absolute",
+    width: 40,
+    height: 40,
+    top: 60,
+    right: 50,
+  },
+
+  pepperoni2: {
+    position: "absolute",
+    width: 38,
+    height: 38,
+    top: 140,
+    left: 60,
+  },
+
+  pepperoni3: {
+    position: "absolute",
+    width: 42,
+    height: 42,
+    bottom: 40,
+    right: 80,
+  },
+
+  /* JALAPEÑOS */
+  jalapeno1: {
+    position: "absolute",
+    width: 45,
+    height: 45,
+    top: 35,
+    left: 100,
+  },
+
+  jalapeno2: {
+    position: "absolute",
+    width: 40,
+    height: 40,
+    top: 130,
+    right: 40,
+  },
+
+  jalapeno3: {
+    position: "absolute",
+    width: 42,
+    height: 42,
+    bottom: 60,
+    left: 70,
+  },
+
+  /* PIÑA */
+  pina1: {
+    position: "absolute",
+    width: 45,
+    height: 45,
+    top: 80,
+    left: 40,
+  },
+
+  pina2: {
+    position: "absolute",
+    width: 42,
+    height: 42,
+    bottom: 70,
+    right: 60,
+  },
+
+  /* PHILADELPHIA */
+  phila1: {
+    position: "absolute",
+    width: 40,
+    height: 40,
+    top: 90,
+    right: 70,
+  },
+
+  phila2: {
+    position: "absolute",
+    width: 42,
+    height: 42,
+    bottom: 50,
+    left: 110,
   },
 
   grid: {
